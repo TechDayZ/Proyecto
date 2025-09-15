711333f5c9d3dc4819175eb41b5eadc27ff2ffa6
luhipaing <socialluhipaing@gmail.com>
2025-09-15 18:32:17 -0300

    Update cooperativa.sql

--- Programacion/sql/cooperativa.sql
+++ Programacion/sql/cooperativa.sql
@@ -1,10 +1,9 @@
-CREATE DATABASE cooperativa;
-USE cooperativa;
-
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    correo VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    telefono VARCHAR(20)
+create table usuario(
+idUser int(8) not null,
+nombre varchar(50) not null,
+fechNac date not null,
+cantFam int(10) not null,
+email varchar(50) not null,
+telefono int(9) not null,
+constraint PK_IDUSER primary key (idUser)
 );

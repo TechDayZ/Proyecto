1d8d96595d49a3605afbafc177d040bb2b722a34
TaoDiaz <taodiazsaires2@gmail.com>
2025-11-18 22:24:31 -0300

    soy boludo

--- Programacion/db.php
+++ Programacion/db.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=cooperativa2;charset=utf8mb4', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=cooperativa;charset=utf8mb4', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
